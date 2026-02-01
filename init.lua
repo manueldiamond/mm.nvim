edcaa530f7234760ff4d7764a14c97f2d6b5369c
manueldiamond <manueldiamondlistowell@gmail.com>
2026-02-01 14:38:51 +0000

    pre ai edits :|

--- init.lua
+++ init.lua
@@ -620,8 +620,8 @@ require('lazy').setup({
         'ts_ls', -- ts_ls for JS/TS
         -- Formatters
         'stylua', -- Lua formatter
-        'prettierd', -- Fast Prettier daemon
-        'prettier', -- Fallback formatter
+        --'prettierd', -- Fast Prettier daemon
+        --'prettier', -- Fallback formatter
         -- Debug Adapter
         --'js-debug-adapter',
       })
@@ -936,6 +936,12 @@ require('lazy').setup({
       })
     end,
   },
+  {
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'VeryLazy',
+    config = true,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -946,18 +952,18 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  -- { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
